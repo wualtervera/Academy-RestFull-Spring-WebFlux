f16675fc6d5b1b3e2eb67ea1341a3fc715bd43be
wualtervera <waltervera693@gmail.com>
2021-12-29 16:47:19 -0500

    add test Student findAll, findById, save

--- src/test/java/com/nttdata/academy/handler/StudenHandlerTest.java
+++ src/test/java/com/nttdata/academy/handler/StudenHandlerTest.java
@@ -1,21 +1,21 @@
 package com.nttdata.academy.handler;
 
-import com.nttdata.academy.AcademyApplication;
 import com.nttdata.academy.model.entity.Student;
 import com.nttdata.academy.services.StudentService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -36,7 +36,7 @@ public class StudenHandlerTest {
 
     @Test
     void findAll() {
-        Flux<Student> studentFlux = Flux.just(s1,s2,s3);
+        Flux<Student> studentFlux = Flux.just(s1, s2, s3);
 
         when(this.service.findAll()).thenReturn(studentFlux);
 
@@ -57,17 +57,57 @@ public class StudenHandlerTest {
 
     @Test
     void findById() {
+        Mono<Student> studentMono = Mono.just(s1);
+
+        when(this.service.findById(any())).thenReturn(studentMono);
+
+        Flux<Student> studentMonoWeb = webTestClient.get().uri(uri.concat("/61c146f39a746217024a9e59"))
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Student.class)
+                .getResponseBody();
+
+        StepVerifier.create(studentMonoWeb)
+                .expectSubscription()
+                .expectNextMatches(s -> s.getNombre().equals("Bill Gates"))
+                .verifyComplete();
     }
 
     @Test
     void save() {
+        Mono<Student> studentMono = Mono.just(s1);
+        when(this.service.save(s1)).thenReturn(studentMono);
+
+        webTestClient.post().uri(uri)
+                .body(Mono.just(s1), Student.class)
+                .exchange()
+                .expectStatus().isCreated();
     }
 
     @Test
     void update() {
+
+        /*Mono<Student> studentMono = Mono.just(s1);
+        when(this.service.update(s1.getId(), s1)).thenReturn(studentMono);
+
+        webTestClient.put().uri(uri.concat("/61c146f39a746217024a9e59"))
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(s1), Student.class)
+                .exchange()
+                .expectStatus().isCreated();*/
     }
 
     @Test
     void delete() {
+        //given(this.service.delete(any())).willReturn(Mono.empty());
+
+
+        /*Mono<Void> voidReturn = Mono.empty();
+        when(this.service.delete(s1.getId())).thenReturn(voidReturn);
+
+        webTestClient.delete().uri(uri.concat("/61c146f39a746217024a9e59"))
+                .exchange()
+                .expectStatus().isNoContent();*/
+
     }
 }
